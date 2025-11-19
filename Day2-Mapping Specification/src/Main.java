3390c0f3ce4f748ac2c06c54faea8157df320bc3
Daham <157113991+ddsere@users.noreply.github.com>
2025-11-19 13:21:26 +0530

    mapping specification

--- Day2-Mapping Specification/src/Main.java
+++ Day2-Mapping Specification/src/Main.java
@@ -1,4 +1,6 @@
-public class Main {
+import jakarta.servlet.http.HttpServlet;
+
+public class Main extends HttpServlet {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
